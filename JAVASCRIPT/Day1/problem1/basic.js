ee400977998b11acebdf3027fd035bc9dfa9efae
sahillagad <sahillagad9@gmail.com>
2023-01-12 08:16:24 +0530

    a
    a

--- JAVASCRIPT/Day1/problem1/basic.js
+++ JAVASCRIPT/Day1/problem1/basic.js
@@ -1,1 +1,53 @@
 console.log("Hello World");
+
+// single line Comment
+/*
+ multipal line Comment
+*/
+
+
+// Javascript Console Statement
+// 1] log
+    console.log("Welome To Javascript");
+
+//2] info
+     console.info("Welome To Javascript")
+
+//3] warn
+    console.warn("Welome To Javascript");
+    
+    
+//4] error 
+    console.error("Welome To Javascript");
+
+//5] table->object
+ var obj={
+    "name" : "sahil",
+    "age"  :21,
+    "add"  :"mumbai"
+ }  
+
+ console.table(obj)
+
+ console.time("time")
+
+ for(var i=0;i<100;i++){
+
+
+
+ }
+ console.timeEnd("time")
+
+
+console.group("a")
+console.log("my new car")
+console.log("my car name kia sonat")
+console.groupCollapsed("Second Group")
+console.log("ram")
+console.log("sahil")
+console.groupEnd("my new car")
+console.log("Kalyan")
+
+console.groupEnd("a")
+
+
---
+++ JAVASCRIPT/Day1/problem2/basic.js
@@ -0,0 +1,31 @@
+// variable Declaration
+/*
+1.Camelcase
+2.use let , const (var is old)
+3. dont start with number
+4. allowed a-z ,  A-Z , 0-9 , _
+*/
+
+// var
+var name = "Sahil";
+console.log(name);
+
+name = "Ram";
+console.log(name);
+
+// let
+let name1 = "Sahil";
+console.log(name1);
+
+name1 = "Ram";
+console.log(name1);
+
+// const
+const name2 = "Sahil";
+console.log(name2);
+
+/*
+name2="Ram"
+console.log(name2)
+const variable we not reassign the value
+*/
