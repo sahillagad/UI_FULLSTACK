c4a4008d482702cffaba6312787520b14fa7a3cc
sahillagad <sahillagad9@gmail.com>
2023-01-17 08:23:47 +0530

    DAY3
    DAY3

---
+++ JAVASCRIPT/Day3/Scope/script.js
@@ -0,0 +1,31 @@
+// example 1
+let a = 10;
+{
+  let a = 20;
+
+  {
+    let a = 30;
+    console.log(a);
+  }
+}
+
+
+// example 2
+let b = 20;
+{
+ let b = 30;
+  {
+    console.log(b);
+  }
+}
+
+// example 3
+let c=30
+{
+  let c=40
+  {
+    let d=60
+
+  }
+ console.log(d)
+}
