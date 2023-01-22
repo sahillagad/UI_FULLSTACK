23a65f5a3aca053fcc94bba9917c56f61cf73d4b
sahillagad <sahillagad9@gmail.com>
2023-01-23 03:05:36 +0530

    Object
    Object

---
+++ JAVASCRIPT/Day1/problem8/script.js
@@ -0,0 +1,31 @@
+// Core  Programing Fundamental
+
+/*
+Object
+*/
+
+var obj={
+    name:"Sahil",
+    age:21,
+    marks:456
+}
+
+console.log(obj)
+
+
+let mobile={
+      brand:"Apple",
+      price:12000,
+      color:"black"      
+}
+
+console.log(mobile);
+console.log(mobile.brand);
+console.log(mobile.price);
+console.log(mobile.color);
+
+console.log(mobile["brand"]);
+console.log(mobile["color"]);
+console.log(mobile["price"]);
+
+
