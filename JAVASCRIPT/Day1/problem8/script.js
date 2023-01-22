fd0a0b0691c46def779a9f3c873605747fe4f76c
sahillagad <sahillagad9@gmail.com>
2023-01-23 04:06:40 +0530

    Day5
    Day5

--- JAVASCRIPT/Day1/problem8/script.js
+++ JAVASCRIPT/Day1/problem8/script.js
@@ -4,20 +4,19 @@
 Object
 */
 
-var obj={
-    name:"Sahil",
-    age:21,
-    marks:456
-}
+var obj = {
+  name: "Sahil",
+  age: 21,
+  marks: 456,
+};
 
-console.log(obj)
+console.log(obj);
 
-
-let mobile={
-      brand:"Apple",
-      price:12000,
-      color:"black"      
-}
+let mobile = {
+  brand: "Apple",
+  price: 12000,
+  color: "black",
+};
 
 console.log(mobile);
 console.log(mobile.brand);
@@ -27,5 +26,31 @@ console.log(mobile.color);
 console.log(mobile["brand"]);
 console.log(mobile["color"]);
 console.log(mobile["price"]);
+
+var empDetail = {
+  empId: 12,
+  empName: "Sahil",
+  empSalary: 45000,
+};
+
+console.log(empDetail);
+// Add Details in The Object
+empDetail.empDepartment = "IT";
+console.log(empDetail);
+
+const student = {
+  roll: 1,
+  name: "Ram",
+  marks: 777,
+};
+
+student.name = 897;
+console.log(student);
+
+student.address = "Mumbai";
+console.log(student);
+
+
+
 
 
---
+++ JAVASCRIPT/Day2/problem4/index.js
@@ -0,0 +1,51 @@
+// Math Operator
+
+const num1=100;
+const num2=10;
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1/num2);
+console.log(num1*num2);
+console.log(num1%num2);
+
+
+var num3=50;
+console.log(num3);
+
+num3=num3+10;
+console.log(num3);
+
+
+num3=num3-10;
+console.log(num3);
+
+num3=num3/10;
+console.log(num3);
+
+num3=num3*10;
+console.log(num3);
+
+num3=num3%2;
+console.log(num3);
+
+
+
+const num4=678;
+console.log(num4);
+
+num4+=400;
+console.log(num4);
+
+
+num4-=100;
+console.log(num4);
+
+num4*=10;
+console.log(num4);
+
+num4/=10;
+console.log(num4);
+
+num4%=2
+console.log(num4);
