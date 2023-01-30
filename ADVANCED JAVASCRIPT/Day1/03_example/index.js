2918447f8da434ea7bacec4af955c48499f6a208
sahillagad <sahillagad9@gmail.com>
2023-01-30 23:46:05 +0530

    Practise3
    Practise3

---
+++ ADVANCED JAVASCRIPT/Day1/03_example/index.js
@@ -0,0 +1,24 @@
+/*
+ Call Back Function 
+*/
+
+/*
+I want Make Coffee It Full Depandent Statement
+1] Get Milk from Shop
+2] Boil The Milk 
+3] Add Sugar AND Coffee
+*/
+
+
+
+const getMilkFromShop=()=>{
+
+    setTimeout(()=>{
+ 
+         console.log("I Get Milk From Shop")
+
+    },2000)
+
+};
+
+const boilMilk=()
