4e78df2c61e1f91449707d5155693c4d66d16648
sahillagad <sahillagad9@gmail.com>
2023-01-31 01:29:18 +0530

    promise with asyncronus Programming

---
+++ ADVANCED JAVASCRIPT/Day1/07_example/index.js
@@ -0,0 +1,58 @@
+/*
+  Promise Function 
+*/
+
+/*
+I want Make Coffee It Full Depandent Statement
+1] Get Milk from Shop
+2] Boil The Milk 
+3] Add Sugar AND Coffee
+*/
+
+var getMilkFromShop = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let isDone = true;
+    if (isDone) {
+      resolve(" 1. Get Milk from Shop");
+    } else {
+      reject("Failed To Get Mailk From Shop");
+    }
+  }, 3000);
+});
+
+var boilMilk = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    var isDone = true;
+    if (isDone) {
+      resolve("2. Boil The Milk ");
+    } else {
+      reject("Failed To Boil The Milk");
+    }
+  }, 1000);
+});
+
+var addSugar = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    var isDone = true;
+    if (isDone) {
+      resolve("3. Add Sugar AND Coffee ");
+    } else {
+      reject("Failed To Add Sugar AND Coffee");
+    }
+  }, 2000);
+});
+
+var getCoffee = () => {
+  getMilkFromShop.then((result) => {
+    console.log("getMilkFromShop Promise Call " + result);
+
+    boilMilk.then((output) => {
+      console.log("boilMilk Promise Call " + output);
+
+      addSugar.then((ans) => {
+        console.log("addSugar : " + ans);
+      });
+    });
+  });
+};
+getCoffee();
